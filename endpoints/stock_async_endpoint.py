e5fbf534a339caabb7785d71c3238108acb90775
Wouter Zorgdrager <zorgdragerw@gmail.com>
2020-05-18 21:56:30 +0200

    Merging with orders.

--- endpoints/stock_async_endpoint.py
+++ endpoints/stock_async_endpoint.py
@@ -1,7 +1,28 @@
+from aiohttp import web
+import asyncio
 
+from async_endpoint import app, send_msg, messages, WORKER_ID
 from stock_pb2 import *
 
-@routes.post('/stock/item/create/{price}')
+# create the logger and configure
+import logging
+
+# define the routes object
+routes_stock = web.RouteTableDef()
+
+FORMAT = '[%(asctime)s] %(levelname)-8s %(message)s'
+logging.basicConfig(level=logging.INFO, format=FORMAT)
+
+logger = logging.getLogger()
+
+# Some parameters to send and read from kafka
+KAFKA_BROKER = "kafka-broker:9092"
+STOCK_TOPIC = "stock"
+STOCK_CREATION_TOPIC = "stock-create"
+STOCK_EVENTS_TOPIC = "stock-events"
+
+
+@routes_stock.post('/stock/item/create/{price}')
 async def stock_create_item(request):
     price = int(request.match_info['price'])
 
