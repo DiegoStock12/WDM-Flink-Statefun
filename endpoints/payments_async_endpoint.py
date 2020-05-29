817b5dbbe48178650959eb07d8e7924a11537920
Ruben Keulemans <r.keulemans@student.tudelft.nl>
2020-05-29 13:20:28 +0200

    Remove unused payments/pay endpoint, remove request_info assignment that is already done by send_msg()

--- endpoints/payments_async_endpoint.py
+++ endpoints/payments_async_endpoint.py
@@ -19,24 +19,6 @@ logger = logging.getLogger()
 KAFKA_BROKER = "kafka-broker:9092"
 PAYMENT_TOPIC = "payment-events"
 
-# This endpoint will never be used, the orders function calls directly
-@routes_payments.post('/payment/pay/{user_id}/{order_id}')
-async def order_pay(request):
-    user_id = int(request.match_info['user_id'])
-    order_id = int(request.match_info['order_id'])
-
-    msg = PaymentRequest()
-    msg.user_id = user_id
-    msg.order_id = order_id
-    msg.request_type = PaymentRequest.RequestType.PAY
-    msg.request_info.request_id = str(uuid.uuid4())
-    msg.request_info.worker_id = 1 #TODO: FIXME
-
-    result = await send_msg(PAYMENT_TOPIC, key='payment_pay', request=msg)
-
-    r_json = json.loads(result)
-    raise web.HTTPOk() if r_json['result'] == 'success' else web.HTTPNotFound()
-
 @routes_payments.post('/payment/cancel/{user_id}/{order_id}')
 async def order_cancel(request):
     user_id = int(request.match_info['user_id'])
@@ -46,8 +28,6 @@ async def order_cancel(request):
     msg.user_id = user_id
     msg.order_id = order_id
     msg.request_type = PaymentRequest.RequestType.CANCEL
-    msg.request_info.request_id = str(uuid.uuid4())
-    msg.request_info.worker_id = 1 #TODO: FIXME
 
     result = await send_msg(PAYMENT_TOPIC, key='payment_pay', request=msg)
 
@@ -61,8 +41,6 @@ async def order_status(request):
     msg = PaymentRequest()
     msg.order_id = order_id
     msg.request_type = PaymentRequest.RequestType.STATUS
-    msg.request_info.request_id = str(uuid.uuid4())
-    msg.request_info.worker_id = 1 #TODO: FIXME
 
     result = await send_msg(PAYMENT_TOPIC, key='payment_pay', request=msg)
 
